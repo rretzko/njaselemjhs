f56cff07683ca4cf66e04c92d2274e47b808df28
MFRHoldings <rick@mfrholdings.com>
2021-12-28 11:01:38 -0500

    New Dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4">
+                <ul>
+                    <li>
+                        <a href="{{ route('administration.index') }}">
+                            Administration
+                        </a>
+                    </li>
+                    <li>Adjudication</li>
+                </ul>
             </div>
         </div>
     </div>
