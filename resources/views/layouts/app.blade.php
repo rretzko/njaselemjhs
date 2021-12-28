1afaab1352daac3854b5e150bd32429d89af9c06
MFRHoldings <rick@mfrholdings.com>
2021-12-28 10:18:44 -0500

    update resources/views/vendor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <style>
+            body{font-size: 1.5rem;}
+        </style>
 
         @livewireStyles
 
