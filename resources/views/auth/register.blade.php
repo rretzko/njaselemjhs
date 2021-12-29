e75ab21b45448ebcc64eb496ccac08a4c25416d9
MFRHoldings <rick@mfrholdings.com>
2021-12-29 08:30:03 -0500

    Add comforming logo elements to login and register forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,28 @@
 <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <x-slot name="logo"></x-slot>
 
         <x-jet-validation-errors class="mb-4" />
+
+        <style>img{height:80px;margin: auto;}</style>
+        <div  style="align-self: center;">
+            <img src="images/tmp_nj_map.jpg"  />
+        </div>
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
