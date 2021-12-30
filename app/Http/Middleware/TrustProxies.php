baae198cb540736006e24be1755788c0ba2d9593
MFRHoldings <rick@mfrholdings.com>
2021-12-30 10:36:50 -0500

    Testing replace proxy with .ondigigitalocean.app address

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = '*';
+    protected $proxies = 'njaselemjhs-4rmtr.ondigitalocean.app';
 
     /**
      * The headers that should be used to detect proxies.
