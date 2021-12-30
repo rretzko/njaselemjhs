09021d824c14de05cdc462dd09a1b3e09be43004
MFRHoldings <rick@mfrholdings.com>
2021-12-30 10:43:21 -0500

    Re-using * for $proxies in TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = 'njaselemjhs-4rmtr.ondigitalocean.app';
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
