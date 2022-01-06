dce6eddd9eb401471fdc5b0d722c815d8d3896f9
MFRHoldings <rick@mfrholdings.com>
2022-01-06 16:24:11 -0500

    Update site owner auth

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4">
                 <ul>
-                    @if((auth()->id() === 1) || (auth()->id() === 66))
+                    @if((auth()->id() === 1) || (auth()->id() === 9)) {{-- Retzko & Reiser --}}
                         <li>
                             <a href="{{ route('administration.index') }}">
                                 Administration
