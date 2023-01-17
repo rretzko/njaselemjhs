4b272ce5c23ceac90a4c529fdba2af81106ad1b8
MFRHoldings <rick@mfrholdings.com>
2023-01-17 14:28:12 -0500

    Fixed download button to isolate currentEvent directors.

--- app/Http/Controllers/Administration/ExportDirectorsController.php
+++ app/Http/Controllers/Administration/ExportDirectorsController.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers\Administration;
 use App\Exports\DirectorsExport;
 use App\Http\Controllers\Controller;
 use App\Models\Director;
+use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ExportDirectorsController extends Controller
 {
     public function export()
     {
-        $directors = new DirectorsExport(
-            Director::orderBy('last')
-                ->orderBy('first')
-                ->get()
-        );
+        $ids = DB::table('director_event')
+            ->where('event_id', Event::currentEvent()->first()->id)
+            ->pluck('user_id');
+
+        $eventDirectors = Director::find($ids)->sortBy('last');
+
+        $directors = new DirectorsExport($eventDirectors);
 
         $datetime = date('Ynd_Gis');
 
